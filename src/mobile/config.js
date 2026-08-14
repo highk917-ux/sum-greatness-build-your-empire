7cc6b76704fee67e807382f59a267baf8b8cc435
highk917-ux <highk917@gmail.com>
2026-08-13 17:44:19 -0700

    Add mobile foundation: src/mobile/config.js

---
+++ src/mobile/config.js
@@ -0,0 +1,21 @@
+export const MOBILE_APP = {
+  appId: 'com.sumgreatness.buildyourempire',
+  entitlement: 'SUM GREATNESS: Build Your Empire Pro',
+  revenueCatProducts: {
+    cashSmall: 'com.sumgreatness.cash.small',
+    cashLarge: 'com.sumgreatness.cash.large',
+    gems100: 'com.sumgreatness.gems.100',
+    founderPack: 'com.sumgreatness.founder',
+    vipMonthly: 'com.sumgreatness.pro.monthly',
+    vipYearly: 'com.sumgreatness.pro.yearly',
+    vipLifetime: 'com.sumgreatness.pro.lifetime'
+  },
+  admobPlacements: {
+    rewardedMain: 'rewarded-main',
+    rewardedMission: 'rewarded-interstitial-mission',
+    shopBanner: 'shop-banner',
+    appOpen: 'app-open'
+  },
+  productionKeysRequired: true,
+  testAdsOnlyDuringDevelopment: true
+};
