07350a4b12f758d8e2b9b2bee0096f63a933f234
highk917-ux <highk917@gmail.com>
2026-08-18 01:01:40 -0700

    Add full-screen landscape splash activity

---
+++ android/app/src/main/java/com/sumgreatness/buildyourempire/FullScreenSplashActivity.java
@@ -0,0 +1,67 @@
+package com.sumgreatness.buildyourempire;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.widget.ImageView;
+
+public class FullScreenSplashActivity extends Activity {
+    private static final long SPLASH_DURATION_MS = 1800L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        enterImmersiveMode();
+
+        ImageView splashImage = new ImageView(this);
+        splashImage.setBackgroundColor(Color.BLACK);
+        splashImage.setImageResource(R.drawable.splash);
+        splashImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        splashImage.setAdjustViewBounds(false);
+        setContentView(splashImage);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            overridePendingTransition(0, 0);
+        }, SPLASH_DURATION_MS);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enterImmersiveMode();
+        }
+    }
+
+    private void enterImmersiveMode() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+}
